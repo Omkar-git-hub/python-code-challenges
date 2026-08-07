dc09a48d054cc9a37046d7868c0df6dcc85cda53
Omkar.Nikam <omkarnikam.work@gmail.com>
2026-08-07 21:24:16 +0530

    feat: Print Your Name

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,11 @@
-def test_hello_world():
-    import io
-    import sys
-    capturedOutput = io.StringIO() 
-    sys.stdout = capturedOutput 
-    exec(open("main.py").read())
-    sys.stdout = sys.__stdout__ 
-    assert capturedOutput.getvalue().strip() == "Hello World"
+import sys
+import io
+import unittest
+from unittest.mock import patch
+from main import *
+
+class TestMain(unittest.TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_name(self, mock_stdout):
+        print("Your Name")
+        self.assertEqual(mock_stdout.getvalue().strip(), "Your Name")
