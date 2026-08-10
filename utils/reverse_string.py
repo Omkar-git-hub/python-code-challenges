261e05bb12319d9a54a82a36acbbc3e0d188c8df
runner <runner@runnervmvrwv9>
2026-08-10 07:19:37 +0000

    feat: Reverse a String

---
+++ utils/reverse_string.py
@@ -0,0 +1,14 @@
+"""
+Module to reverse a given string.
+"""
+def reverse_string(input_str: str) -> str:
+    """
+    Reverses the input string.
+
+    Args:
+        input_str (str): The string to be reversed.
+
+    Returns:
+        str: The reversed string.
+    """
+    return input_str[::-1]
