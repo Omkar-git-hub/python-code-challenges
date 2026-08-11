b7caecb11e3c858be1ead2c27dc99b52bbafcf26
runner <runner@runnervmvrwv9>
2026-08-11 06:56:40 +0000

    feat: Sum of List Elements

---
+++ utils/sum_of_list_elements.py
@@ -0,0 +1,14 @@
+"""
+This module provides a function to calculate the sum of elements in a list.
+"""
+def sum_of_list_elements(input_list):
+    """
+    Calculate the sum of elements in a list.
+
+    Args:
+        input_list (list): A list of numbers.
+
+    Returns:
+        int: The sum of the elements in the list.
+    """
+    return sum(input_list)
